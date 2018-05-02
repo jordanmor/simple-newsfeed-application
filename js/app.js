7212296871d227764235e8f5c9a064c600d675d4
Jordan Mor <jordanmor@gmail.com>
2018-05-02 07:57:14 -0400

    Create label/checkbox to allow PDF downloads in javascript instead of HTML

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const $odd = $('a:odd');
 const $secureLinks = $('a[href^="https://"]');
 const $pdfs = $('a[href$=".pdf"]');
+const $pdfCheckbox = $("<label><input type='checkbox'> Allow PDF downloads</label>");
+
+$('#links').append($pdfCheckbox);
 
 $secureLinks
 	.attr('target', '_blank')
@@ -10,10 +13,9 @@ $pdfs
 	.attr('download', true)
 	.addClass('pdf');
 
-	$pdfs.on('click', function(event) {
-		if ($(':checked').length === 0) {
-			event.preventDefault();
-			alert('Please check the box to allow PDF downloads');
-		}
-	});
-	
+$pdfs.on('click', function(event) {
+	if ($(':checked').length === 0) {
+		event.preventDefault();
+		alert('Please check the box to allow PDF downloads');
+	}
+});
